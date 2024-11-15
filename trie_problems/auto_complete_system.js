ac0f2d87c7dae474ad499e65cd03f72f38effbf6
Vishnu P S <vishnu13three@gamil.com>
2024-11-15 20:54:03 +0530

    autocompllete iin trei complete

--- trie_problems/auto_complete_system.js
+++ trie_problems/auto_complete_system.js
@@ -21,4 +21,49 @@ class AutoCompleteSystem{
         node.isEndOfWord = true;
         node.frequency +=1;
     }
+    autocomplete(prefix){
+        let node = this.root;
+        for(let char of prefix){
+            if(!node.children[char]){
+                return [];
+
+            }
+            node = node.children[char];
+        }
+        const suggestions =[];
+
+        const dfs = (currentNode,path) => {
+            if(currentNode.isEndOfWord){
+                suggestions.push({word:path, frequency :currentNode.frequency});
+            }
+            for(let [char, childNode] of Object.entries(currentNode.children)){
+                dfs(childNode, path+ char);
+
+            }
+        };
+        dfs(node, prefix);
+
+        suggestions.sort((a,b) =>{
+            if(b.frequency === a.frequency){
+                return a.word.localeCompare(b.word);
+            }
+            return b.frequency - a.frequency;
+        });
+        return suggestions.map(s=> s.word);
+
+    }
 }
+
+const system = new AutoCompleteSystem();
+system.insert("apple");
+system.insert("app");
+system.insert("application");
+system.insert("app");
+system.insert("apple");
+system.insert("appetite");
+system.insert("apple");
+system.insert("app");
+
+console.log(system.autocomplete("app"));
+console.log(system.autocomplete("appl"));
+console.log(system.autocomplete("xyz"));
