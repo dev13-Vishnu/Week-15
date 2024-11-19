be21cf963b453e19b9e5f5d88b2d7b8740341499
Vishnu P S <vishnu13three@gamil.com>
2024-11-19 10:55:30 +0530

    tree.js updated addid delete method

--- tree_problems/tree.js
+++ tree_problems/tree.js
@@ -1,4 +1,4 @@
-class TreeNode {
+class TreeNode{
     constructor(value){
         this.value = value;
         this.children = [];
@@ -7,52 +7,82 @@ class TreeNode {
         this.children.push(child);
     }
 }
-
-class Tree{
+class Tree {
     constructor(rootValue){
         this.root = new TreeNode(rootValue);
     }
-    insert(parentValue,childValue){
-        const parentNode = this.find(this.root,parentValue);
+    insert(parentValue, childValue){
+        const parentNode = this.find(this.root, parentValue);
         if(parentNode){
-            parentNode.addChild( new TreeNode(childValue));
+            parentNode.addChild(new TreeNode(childValue));
         } else {
-            console.log(`Parent node with the value ${parentNode} not found.`);
+            console.log(`parent node with the value ${parentValue} did not found.`);
         }
     }
     find(node,value){
-        if(node.value === value){
-            return node;
-        }
+        if(node.value === value) return node;
         for(let child of node.children){
-            const result = this.find(child,value);
+            let result = this.find(child, value);
             if(result){
                 return result;
             }
         }
         return null;
     }
-    print(node = this.root , level =0){
-        console.log(" ".repeat(level) + node.value);
-        for( const child of node.children){
-            this.print(child, level+1);
+    print(node = this.root, level = 1){
+        console.log(" ".repeat(level*2)+ node.value);
+        for(let child of node.children){
+            this.print(child, level +1);
         }
     }
     search(value){
         const node = this.find(this.root, value);
         return node !== null;
     }
+    delete(value){
+        if(this.root.value === value){
+            console.log("cannot delete the root node.");
+            return ;
+        }
+        const parentNode = this.findParent(this.root, value)
+        if(!parentNode){
+            console.log(`parent Node with value '${value} not found.`);
+            return;
+        }
+        parentNode.children = parentNode.children.filter(child => child.value !== value);
+    }
+    findParent(node,value){
+        if(!node) return null;
+        for(let child of node.children){
+            if(child.value === value){
+                return node;
+            }
+        }
+        for(let child of node.children){
+            let result = this.findParent(child,value);
+            if(result) return result;
+        }
+        return null;
+    }
 }
-
-const myTree  = new Tree("root");
+const myTree = new Tree("root");
 myTree.insert("root","child1");
 myTree.insert("root","child2");
 myTree.insert("child1","child1.1");
 myTree.insert("child1", "child1.2");
-myTree.insert("child2","child2.1");
-console.log("search child1:",myTree.search("child1"));
-console.log("search child3:", myTree.search("child3"));
+myTree.insert("child2", "child2.1");
 console.log("tree structure");
 myTree.print();
+console.log("searched Item",myTree.search("child1"));
+console.log("searched Item",myTree.search("child3"));
 const node = myTree.find(myTree.root,"child1");
-console.log("found Node:",node ? node.value:"node.found");
+console.log("found Node:", node ? node.value : "node.found");
+console.log("Deleting child");
+myTree.delete("child1");
+myTree.print();
+console.log("Deleting child3 (non - existent)");
+myTree.delete("child3");
+myTree.print();
+console.log("Trying to delete the root node");
+myTree.delete("root");
+myTree.print();
